f08206fbdff4b4088c50a84cd0b731635bba2dbd
Mustafa Yumurtacı <mustafa.yumurtaci@trendyol.com>
2021-12-11 01:09:01 +0300

    add ch4

--- src/main/java/org/example/tdd/by/examples/Dollar.java
+++ src/main/java/org/example/tdd/by/examples/Dollar.java
@@ -1,7 +1,7 @@
 package org.example.tdd.by.examples;
 
 class Dollar {
-    int amount;
+    private int amount;
 
     public Dollar(int amount) {
         this.amount = amount;
--- src/test/java/org/example/tdd/by/examples/DollarTest.java
+++ src/test/java/org/example/tdd/by/examples/DollarTest.java
@@ -9,8 +9,7 @@ class DollarTest {
     @Test
     void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
+        assertEquals(new Dollar(10), five.times(2));
     }
 
 //    @Test
@@ -25,10 +24,8 @@ class DollarTest {
     @Test
     void testMultiplication3() {
         Dollar five = new Dollar(5);
-        Dollar product = five.times(2);
-        assertEquals(10, product.amount);
-        product = five.times(3);
-        assertEquals(15, product.amount);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
     }
 
     @Test
