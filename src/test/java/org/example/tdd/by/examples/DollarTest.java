cb777065cdc712103905553741827ba048466048
Mustafa Yumurtacı <mustafa.yumurtaci@trendyol.com>
2021-12-10 19:31:31 +0300

    add ch3

--- src/test/java/org/example/tdd/by/examples/DollarTest.java
+++ src/test/java/org/example/tdd/by/examples/DollarTest.java
@@ -2,7 +2,7 @@ package org.example.tdd.by.examples;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class DollarTest {
 
@@ -23,11 +23,18 @@ class DollarTest {
 //    }
 
     @Test
-    public void testMultiplication3() {
+    void testMultiplication3() {
         Dollar five = new Dollar(5);
         Dollar product = five.times(2);
         assertEquals(10, product.amount);
         product = five.times(3);
         assertEquals(15, product.amount);
+    }
+
+    @Test
+    void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
+
     }
 }
